708b2c41b413cde5b7614caeb513c8664b3df8a1
Igor Kowalik <igik451@gmail.com>
2026-09-16 20:29:48 +0200

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # number of graph components
+        # create a graph then do a bfs search
+
+        # traverse through the matrix -> O(nm)
+        rows = len(grid)
+        cols = len(grid[0])
+
+        visited = set()
+        islands = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] != "1" or (i, j) in visited:
+                    continue
+                visited.add((i, j))
+                islands += 1
+
+                stack = [(i, j)]
+                while stack:
+                    currRow, currCol = stack.pop()
+                    for newRow, newCol in ((currRow - 1, currCol), (currRow + 1, currCol), (currRow, currCol - 1), (currRow, currCol + 1)):
+                        if (
+                            0 <= newRow < rows
+                            and 0 <= newCol < cols
+                            and grid[newRow][newCol] == "1"
+                            and (newRow, newCol) not in visited
+                        ):
+                            stack.append((newRow, newCol))
+                            visited.add((newRow, newCol))
+        return islands
