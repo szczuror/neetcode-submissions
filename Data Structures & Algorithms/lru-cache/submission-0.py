470ee3bc2f9dd351ba1e3413cd28e36cddd5926a
Igor Kowalik <igik451@gmail.com>
2026-09-21 21:13:07 +0200

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def _remove(self, node: Node) -> None:
+        prev = node.prev
+        next_node = node.next
+        prev.next = next_node
+        next_node.prev = prev
+    
+    def _insert(self, node: Node) -> None:
+        prev_node = self.right.prev
+        prev_node.next = node
+        node.prev = prev_node
+        node.next = self.right
+        self.right.prev = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key] # ? IDK TBH
+        self._remove(node)
+        self._insert(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        new_node = Node(key, value)
+
+        self._insert(new_node)
+        self.cache[key] = new_node
+
+        if len(self.cache) > self.capacity:
+            toDel = self.left.next
+            self._remove(toDel)
+            del self.cache[toDel.key]
